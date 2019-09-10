d4a1514dd6a687b8d57aca789321ebec6b45ce89
Giabach <giabach@gmail.com>
2019-09-10 16:35:54 +1000

    Update TextRecognizerAsyncTask.java
    test

--- android/src/general/java/org/reactnative/camera/tasks/TextRecognizerAsyncTask.java
+++ android/src/general/java/org/reactnative/camera/tasks/TextRecognizerAsyncTask.java
@@ -64,7 +64,7 @@ public class TextRecognizerAsyncTask extends android.os.AsyncTask<Void, Void, Sp
 
   @Override
   protected SparseArray<TextBlock> doInBackground(Void... ignored) {
-    return null;
+    mDelegate = null;
     if (isCancelled() || mDelegate == null) {
       return null;
     }
